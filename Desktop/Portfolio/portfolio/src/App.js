517d9ad65ab55462e11c2b8c6927008f39835d15
Shannon Kendall <snkendall@gmail.com>
2018-02-07 19:56:53 -0500

    contact icons are there and styled

--- Desktop/Portfolio/portfolio/src/App.js
+++ Desktop/Portfolio/portfolio/src/App.js
@@ -7,17 +7,52 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <div className="logo-container">
             <FontAwesome
-              className='fas fa-cog slow-spin images'
+              className='fas fa-cog slow-spin images yellow-accent'
               name='headshotSpinner'
               size='10x' 
             />
             <img className="headshot images" src="./images/cropped.jpg" />
           </div>
-          {/* <h1 className="App-title">Oh Hello</h1> */}
         </header>
+        <div className='contact-container'>
+          <a className="contact-button" href='mailto:snkendall@gmail.com'>
+              <FontAwesome 
+                className='fas fa-envelope contact-button'
+                name='emailIcon'
+                size='md'
+              />
+          </a>
+          <a className="contact-button" href='https://github.com/snkendall'>
+              <FontAwesome 
+                className='fab fa-github contact-button'
+                name='gitHubIcon'
+                size='md'
+              />
+          </a>
+          <a className="contact-button" href='https://www.linkedin.com/in/shannonkendall-/'>
+              <FontAwesome 
+                className='fab fa-linkedin contact-button'
+                name='linkedInIcon'
+                size='md'
+              />
+          </a>
+          <a className="contact-button" href='https://stackoverflow.com/users/7964786/snkendall?tab=profile'>
+              <FontAwesome 
+                className='fab fa-stack-overflow contact-button'
+                name='stackOverflowIcon'
+                size='md'
+              />
+          </a>
+          <a className="contact-button" href='https://twitter.com/snkendall_'>
+              <FontAwesome 
+                className='fab fa-twitter contact-button'
+                name='twitterIcon'
+                size='md'
+              />
+          </a>
+        </div>  
         <p className="App-intro">
           I'm a Full Stack Engineer. Amongst other things.
         </p>
